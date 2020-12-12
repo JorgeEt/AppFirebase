9273ea198942eeee4c9aab2b47dbe0f883045b6d
JorgeEt <75908209+JorgeEt@users.noreply.github.com>
2020-12-12 12:30:28 -0600

    Add files via upload

---
+++ src/main/java/model/persona.java
@@ -0,0 +1,51 @@
+package model;
+
+public class persona {
+
+    public String uid;
+    public String Nombre;
+    public String Apellidos;
+    public String Correo;
+
+    public persona() {
+
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        Apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return Nombre;
+    }
+
+}
